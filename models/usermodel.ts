18dfda8e457abe971f646fa2c765b58ddebeb6fc
ahmed hassan <you@example.com>
2025-07-07 19:00:52 +0500

    yell

--- models/usermodel.ts
+++ models/usermodel.ts
@@ -65,3 +65,13 @@ export const validateRegister = (data: {
   });
   return schema.validate(data);
 };
+export const validateLogin = (data: { 
+  email: string; 
+  password: string 
+}) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required().label("Email"),
+    password: Joi.string().required().label("Password"),
+  });
+  return schema.validate(data);
+};
